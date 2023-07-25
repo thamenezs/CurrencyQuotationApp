c9043edab4b4733f3f0ab31b42b40a092bfb2a88
Thais Menezes <thais.mas@hotmail.com>
2023-07-25 11:34:51 -0300

    feat: first commit with title component

---
+++ CurrencyQuotationApp/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extension.swift
+//  CurrencyQuotationApp
+//
+//  Created by Espezzialy Souza on 09/10/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    static let purpleColor = UIColor(named: "purpleColor")
+}
--- CurrencyQuotationApp/ViewController.swift
+++ CurrencyQuotationApp/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private lazy var titleLabel:UILabel = {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = "Converter para Real"
+        title.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        title.textColor = UIColor.purpleColor
+        title.textAlignment = .center
+        return title
+    }()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+            super.viewDidLoad()
+            setupView()
 
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+        }
+
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+        }
+        
+        override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+        }
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+        }
+    
+    private func setupView(){
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+    }
+    
+    private func setConstraints(){
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 65),
+        ])
+    }
 
 }
 
