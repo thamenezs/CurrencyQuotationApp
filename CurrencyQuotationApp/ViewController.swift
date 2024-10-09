4e098bf2f824084939dd01287009a1d282018203
Thais Menezes <thais.mas@hotmail.com>
2024-10-09 15:39:31 -0300

    Initial Commit

---
+++ CurrencyQuotationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyQuotationApp
+//
+//  Created by Espezzialy Souza on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
